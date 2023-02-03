cb33b86f92a7671cd17ea13a4c9ec95337e2ed48
hanbing <1411877187@qq.com>
2023-02-03 16:45:10 +0800

    这是第一次修改

---
+++ 7_WebAPI&vite/demo_02/src/m1.js
@@ -0,0 +1,19 @@
+export default {
+    setH1(){
+        document.body.insertAdjacentHTML("beforeend", "<h1>你好 webpack</h1>")
+        
+    }
+}
+
+
+
+
+
+// import $ from 'jquery';
+
+// export default {
+//     setH1(){
+//         $("body").append("<h1>你好！ 这是引入jquery后打包的</h1>")
+//     }
+    
+// }
---
+++ 7_WebAPI&vite/demo_02/src/m2.js
@@ -0,0 +1,9 @@
+export default {
+    setHello(){
+        console.log("你好 webpack");
+    },
+    a: 10,
+    b: 20
+}
+
+// console.log("hello");
